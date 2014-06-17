c424320f6c825bb39942a5e88528b5f064dc6a4c
nljohnso <niklaus.johnson@gmail.com>
2014-06-17 14:02:03 -0600

    Create TCPSender.java

---
+++ src/cs455/overlay/transport/TCPSender.java
@@ -0,0 +1,19 @@
+package cs455.overlay.transport;
+
+import java.io.*;
+import java.net.Socket;
+
+public class TCPSender {
+	private DataOutputStream dout;
+	
+	public TCPSender(Socket socket) throws IOException {
+		dout = new DataOutputStream(socket.getOutputStream());
+	}
+
+	public void sendData(byte[] dataToSend) throws IOException {
+		int dataLength = dataToSend.length;
+		dout.writeInt(dataLength);
+		dout.write(dataToSend, 0, dataLength);
+		dout.flush();
+	}
+}
