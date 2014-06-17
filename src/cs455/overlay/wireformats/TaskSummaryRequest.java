071f322dffd6566533afb65d887fdb652cba1b80
nljohnso <niklaus.johnson@gmail.com>
2014-06-17 14:07:07 -0600

    Create TaskSummaryRequest.java

---
+++ src/cs455/overlay/wireformats/TaskSummaryRequest.java
@@ -0,0 +1,63 @@
+package cs455.overlay.wireformats;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+import cs455.overlay.wireformats.Message;
+
+public class TaskSummaryRequest extends Message {
+
+	public TaskSummaryRequest() {
+		super(TASK_SUMMARY_REQ);
+	}
+	
+	public TaskSummaryRequest(int type, byte[] bytes) {
+		super(type);
+		try {
+			unMarshal(bytes);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public byte[] marshal() throws IOException {
+		byte[] marshalledBytes = null;
+		ByteArrayOutputStream baOutputStream = new ByteArrayOutputStream();
+		DataOutputStream dout = new DataOutputStream(new BufferedOutputStream(baOutputStream));
+		
+		dout.writeInt(getSize());
+		dout.writeInt(getType());
+
+		dout.flush();
+		marshalledBytes = baOutputStream.toByteArray();
+		baOutputStream.close();
+		dout.close();
+		
+		return marshalledBytes;
+	}
+	
+	@Override
+	public void unMarshal(byte[] marshalledBytes) throws IOException {
+		ByteArrayInputStream baInputStream =
+		new ByteArrayInputStream(marshalledBytes);
+		DataInputStream din =
+		new DataInputStream(new BufferedInputStream(baInputStream));
+		
+		setType(din.readInt());
+
+		baInputStream.close();
+		din.close();
+	}
+
+	@Override
+	protected int getSize() {
+		return 4;
+	}
+
+}
